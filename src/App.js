bcd9570bf063271fc790ac31657f6828b3499422
Hugo <blackfoxmmm@gmail.com>
2022-02-03 17:39:30 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React from "react";
 
 function App() {
+  const elName = React.useRef(null);
+  const elPhone = React.useRef(null);
+  const elEmail = React.useRef(null);
+  const elPassword = React.useRef(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:8030/newUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user_name: elName.current.value,
+        user_phone: elPhone.current.value,
+        user_email: elEmail.current.value,
+        user_password: elPassword.current.value,
+      }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+    elName.current.value = null;
+    elPhone.current.value = null;
+    elEmail.current.value = null;
+    elPassword.current.value = null;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <div className="container">
+          <form onSubmit={handleSubmit} className="rest-form">
+            <h2>Sing in</h2>
+            <input
+              required
+              type="text"
+              autoComplete="off"
+              placeholder="Name"
+              className="rest-name"
+              ref={elName}
+            />
+            <input
+              required
+              type="text"
+              autoComplete="off"
+              placeholder="Phone number"
+              className="rest-name"
+              ref={elPhone}
+            />
+            <input
+              required
+              type="email"
+              autoComplete="off"
+              placeholder="Email"
+              className="rest-name"
+              ref={elEmail}
+            />
+            <input
+              required
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              className="rest-name"
+              ref={elPassword}
+            />
+
+            <button>Submit</button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
